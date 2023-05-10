31ba7e91e1834de05326a629f00fcaca667caf18
“Yang” <704404264@qq.com>
2023-05-10 08:58:23 +0800

    20230510

--- index.js
+++ index.js
@@ -44,23 +44,28 @@ const qd = () => {
     url: config.signInUrl,
     method: "post",
     headers: config.headers,
-  }).then((res) => {
-    if (res.data.err_no == 0) {
-      console.log(
-        `签到成功,获取${res.data.data.incr_point}矿石,总计${res.data.data.sum_point}矿石`
-      );
-    } else {
-      console.log(res.data.err_msg);
-    }
-    //查询免费签到次数
-    setTimeout(() => {
-      freecjCount();
-    }, Math.random() * 1000 * 20);
-  });
+  })
+    .then((res) => {
+      if (res.data.err_no == 0) {
+        console.log(
+          `签到成功,获取${res.data.data.incr_point}矿石,总计${res.data.data.sum_point}矿石`
+        );
+      } else {
+        console.log(res.data.err_msg);
+      }
+      //查询免费签到次数
+      setTimeout(() => {
+        freecjCount();
+      }, (Math.random() + 1) * 1000 * 20);
+    })
+    .catch(() => {
+      console.log("签到错误");
+    });
 };
 
 //定时任务
-schedule.scheduleJob("45 8 1 * * *", () => {
+schedule.scheduleJob("0 45 8 * * *", () => {
+  console.log(new Date() + "开始执行");
   setTimeout(() => {
     qd();
   }, Math.random() * 10 * 60 * 1000);
